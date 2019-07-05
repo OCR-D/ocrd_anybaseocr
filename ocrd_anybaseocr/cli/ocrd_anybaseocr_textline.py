689e12cf2d43ea22536dce589d6f237fa14b964d
Rakshith <pritam.rakshi@gmail.com>
2019-07-05 17:14:55 +0200

    textline cli version

--- ocrd_anybaseocr/cli/ocrd_anybaseocr_textline.py
+++ ocrd_anybaseocr/cli/ocrd_anybaseocr_textline.py
@@ -22,10 +22,17 @@ import subprocess
 from ..utils import parseXML, write_to_xml, print_info, parse_params_with_defaults, print_error
 from ..constants import OCRD_TOOL
 
-class OcrdAnybaseocrTextline():
+from ocrd import Processor
+from ocrd_modelfactory import page_from_file
+from ocrd_models.ocrd_page import to_xml
+from ocrd_utils import concat_padded
 
-	def __init__(self, param):
-		self.param = param
+class OcrdAnybaseocrTextline(Processor):
+
+	def __init__(self, *args, **kwargs):
+		kwargs['ocrd_tool'] = OCRD_TOOL['tools']['ocrd-anybaseocr-textline']
+		kwargs['version'] = OCRD_TOOL['version']
+		super(OcrdAnybaseocrTextline, self).__init__(*args, **kwargs)
 
 	def addzeros(self, file):
 		F = open(file, "r")
@@ -38,103 +45,93 @@ class OcrdAnybaseocrTextline():
 			d += " 0 0 0 0\n"
 			F.write(d)
 
-	def textline(self, arg):
-		image = ocrolib.read_image_binary(arg)
-		height, width = image.shape
-		H = height
-		W = width
-		base,_ = ocrolib.allsplitext(arg)
-		base2 = os.path.splitext(arg)[0]
+	def process(self):
+		for (n, input_file) in enumerate(self.input_files):            
+			pcgts = page_from_file(self.workspace.download_file(input_file))                
+			binImg = self.workspace.resolve_image_as_pil(pcgts.get_Page().imageFilename)
+			# I: binarized-input-image; imftext: output-text-portion.png; imfimage: output-image-portion.png
+			fname = pcgts.get_Page().imageFilename        
+			image = ocrolib.read_image_binary(fname)
+			height, width = image.shape
+			H = height
+			W = width
+			base,_ = ocrolib.allsplitext(fname)
+			base2 = os.path.splitext(fname)[0]
 
-		if not os.path.exists("%s/lines" % base) :
-			os.system("mkdir -p %s/lines" % base)
-			#if os.path.exists(base2 + ".ts.png") :
-			#    f = ocrolib.read_image_binary(base2 + ".ts.png")
-			#    height, width = f.shape
-			#    os.system("python "+args.libpath+"/anyBaseOCR-nlbin.py %s.pf.bin.png" % base2)
-			#else:
-			#    os.system("python "+args.libpath+"/anyBaseOCR-nlbin.py %s" % arg)
-			#print("convert %s.ts.png %s/block-000.bin.png" % (base,base))
-			#os.system("convert %s.ts.png %s/block-000.bin.png" % (base,base))
-			#os.system("rm %s.bin.png %s.nrm.png" % (base, base))
-			file = open('%s/sorted_cuts.dat' % base, 'w')
-			l = "0 0 " + str(int(width)) + " " + str(int(height)) + " 0 0 0 0\n"
-			file.write(l)
-			file.close()
-
-		#if not os.path.exists("%s/lines" % base) :
-		#    os.system("mkdir %s/lines" % base)
-
-		blockarray = []
-		if os.path.exists(base + "/sorted_cuts.dat") :
-			blocks = open(base + "/sorted_cuts.dat", "r")
-			i = 0
-			for block in blocks:
-				words = block.split()
-				blockarray.append((int(words[0]), -int(words[1]),int(words[2]), int(words[3]), i))
-				i += 1;
-		else:
-			blockarray.append((0, 0, width, height, 0))
-
-		i = 0
-		j = 0
-		lines=[]
-		for block in blockarray:
-			(x0, y0, x1, y1, i) = block
-			y0 = -y0
-			#blockImage = "%s/block-%03d" % (base, i)
-			os.system("convert %s.ts.png %s/temp.png" % (base, base))
-			img = Image.open("%s.ts.png" % base, 'r')
-			img_w, img_h = img.size
-			background = Image.new('RGBA', (W, H), (255, 255, 255, 255))
-			bg_w, bg_h = background.size
-			offX = (bg_w - img_w) // 2
-			offY = (bg_h - img_h) // 2
-			offset = (offX, offY)
-			background.paste(img, offset)
-			background.save("%s/temp.png" % base)
-			command = "python "+self.param['libpath']+"/cli/anyBaseOCR-gpageseg.py %s/temp.png -n --minscale %f --maxlines %f --scale %f --hscale %f --vscale %f --threshold %f --noise %d --maxseps %d --sepwiden %d --maxcolseps %d --csminaspect %f --csminheight %f -p %d -e %d -Q %d" % (base, self.param['minscale'], self.param['maxlines'], self.param['scale'], self.param['hscale'], self.param['vscale'], self.param['threshold'], self.param['noise'], self.param['maxseps'], self.param['sepwiden'], self.param['maxcolseps'], self.param['csminaspect'], self.param['csminheight'], self.param['pad'], self.param['expand'], self.param['parallel'])			
-			if(self.param['blackseps']):
-				command = command + " -b"
-			if(self.param['usegauss']):
-				command = command + " --usegauss"
-			os.system(command)
-			pseg = ocrolib.read_page_segmentation("%s/temp.pseg.png" % base)
-			regions = ocrolib.RegionExtractor()
-			regions.setPageLines(pseg)
-			file = open('%s/sorted_lines.dat' % base, 'w')
-			for h in range(1,regions.length()):
-				id = regions.id(h)
-				y0,x0,y1,x1 = regions.bbox(h)
-				l = str(int(x0 - offX)) + " " + str(int(img_h - (y1 - offY))) + " " + str(int(x1 - offX)) + " " + str(int(img_h - (y0 - offY))) + " 0 0 0 0\n"
+			if not os.path.exists("%s/lines" % base) :
+				os.system("mkdir -p %s/lines" % base)
+				#if os.path.exists(base2 + ".ts.png") :
+				#    f = ocrolib.read_image_binary(base2 + ".ts.png")
+				#    height, width = f.shape
+				#    os.system("python "+args.libpath+"/anyBaseOCR-nlbin.py %s.pf.bin.png" % base2)
+				#else:
+				#    os.system("python "+args.libpath+"/anyBaseOCR-nlbin.py %s" % arg)
+				#print("convert %s.ts.png %s/block-000.bin.png" % (base,base))
+				#os.system("convert %s.ts.png %s/block-000.bin.png" % (base,base))
+				#os.system("rm %s.bin.png %s.nrm.png" % (base, base))
+				file = open('%s/sorted_cuts.dat' % base, 'w')
+				l = "0 0 " + str(int(width)) + " " + str(int(height)) + " 0 0 0 0\n"
 				file.write(l)
-			filelist = glob.glob("%s/temp/*" % base)
-			for infile in sorted(filelist): 
-				os.system("convert %s %s/lines/01%02x%02x.bin.png" % (infile, base, i + 1, j + 1))
-				lines.append("%s/lines/01%02x%02x.bin.png" % (base, i + 1, j + 1))
-				j += 1
-			os.system("rm -r %s/temp/" % base)
-			os.system("rm %s/temp.png %s/temp.pseg.png" % (base, base))
-			i += 1
-		return lines
+				file.close()
+
+			#if not os.path.exists("%s/lines" % base) :
+			#    os.system("mkdir %s/lines" % base)
+
+			blockarray = []
+			if os.path.exists(base + "/sorted_cuts.dat") :
+				blocks = open(base + "/sorted_cuts.dat", "r")
+				i = 0
+				for block in blocks:
+					words = block.split()
+					blockarray.append((int(words[0]), -int(words[1]),int(words[2]), int(words[3]), i))
+					i += 1;
+			else:
+				blockarray.append((0, 0, width, height, 0))
+
+			i = 0
+			j = 0
+			lines=[]
+			for block in blockarray:
+				(x0, y0, x1, y1, i) = block
+				y0 = -y0
+				#blockImage = "%s/block-%03d" % (base, i)
+				os.system("convert %s.ts.png %s/temp.png" % (base, base))
+				img = Image.open("%s.ts.png" % base, 'r')
+				img_w, img_h = img.size
+				background = Image.new('RGBA', (W, H), (255, 255, 255, 255))
+				bg_w, bg_h = background.size
+				offX = (bg_w - img_w) // 2
+				offY = (bg_h - img_h) // 2
+				offset = (offX, offY)
+				background.paste(img, offset)
+				background.save("%s/temp.png" % base)
+				command = "python "+self.parameter['libpath']+"/cli/anyBaseOCR-gpageseg.py %s/temp.png -n --minscale %f --maxlines %f --scale %f --hscale %f --vscale %f --threshold %f --noise %d --maxseps %d --sepwiden %d --maxcolseps %d --csminaspect %f --csminheight %f -p %d -e %d -Q %d" % (base, self.parameter['minscale'], self.parameter['maxlines'], self.parameter['scale'], self.parameter['hscale'], self.parameter['vscale'], self.parameter['threshold'], self.parameter['noise'], self.parameter['maxseps'], self.parameter['sepwiden'], self.parameter['maxcolseps'], self.parameter['csminaspect'], self.parameter['csminheight'], self.parameter['pad'], self.parameter['expand'], self.parameter['parallel'])	
+				if(self.parameter['blackseps']):
+					command = command + " -b"
+				if(self.parameter['usegauss']):
+					command = command + " --usegauss"
+				os.system(command)
+				pseg = ocrolib.read_page_segmentation("%s/temp.pseg.png" % base)
+				regions = ocrolib.RegionExtractor()
+				regions.setPageLines(pseg)
+				file = open('%s/sorted_lines.dat' % base, 'w')
+				for h in range(1,regions.length()):
+					id = regions.id(h)
+					y0,x0,y1,x1 = regions.bbox(h)
+					l = str(int(x0 - offX)) + " " + str(int(img_h - (y1 - offY))) + " " + str(int(x1 - offX)) + " " + str(int(img_h - (y0 - offY))) + " 0 0 0 0\n"
+					file.write(l)
+				filelist = glob.glob("%s/temp/*" % base)
+				for infile in sorted(filelist): 
+					os.system("convert %s %s/lines/01%02x%02x.bin.png" % (infile, base, i + 1, j + 1))
+					lines.append("%s/lines/01%02x%02x.bin.png" % (base, i + 1, j + 1))
+					j += 1
+				os.system("rm -r %s/temp/" % base)
+				os.system("rm %s/temp.png %s/temp.pseg.png" % (base, base))
+				i += 1
+			#return lines
+
 
 '''
-
-	if __name__ == "__main__":
-		myparser = ParserAnybaseocr()
-		args = myparser.get_parameters('ocrd-anybaseocr-textline')
-
-		files = myparser.parseXML()
-		fname = []
-		block=[]
-		for i, f in enumerate(files):
-			myparser.print_info("Process file: %s" % str(f))
-			block.append(textline(str(f)))
-			fname.append(str(f))
-
-		myparser.write_to_xml(fname, 'TL_', block)		
-'''
-
 def main():
 	parser = argparse.ArgumentParser("""
 	Image Text Line Segmentation.
@@ -179,3 +176,5 @@ def main():
 		block.append(textline.textline(str(fname)))
 		fnames.append(str(fname))
 	write_to_xml(fnames, args.mets, args.Output, args.OutputMets, args.work)
+
+'''	
