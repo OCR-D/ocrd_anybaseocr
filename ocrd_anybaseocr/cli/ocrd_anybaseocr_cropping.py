3c8e1ace77428ca8ba5e3aa96ffa952105061713
Konstantin Baierer <unixprog@gmail.com>
2019-12-04 14:59:41 +0100

    cropping: output_file_grp -> page_grp

--- ocrd_anybaseocr/cli/ocrd_anybaseocr_cropping.py
+++ ocrd_anybaseocr/cli/ocrd_anybaseocr_cropping.py
@@ -418,9 +418,9 @@ class OcrdAnybaseocrCropper(Processor):
         """Performs border detection on the workspace. """
         try:
             print("OUTPUT FILE ", self.output_file_grp)
-            page_grp, self.image_grp = self.output_file_grp.split(',')
+            self.page_grp, self.image_grp = self.output_file_grp.split(',')
         except ValueError:
-            page_grp = self.output_file_grp
+            self.page_grp = self.output_file_grp
             self.image_grp = FALLBACK_IMAGE_GRP
             LOG.info(
                 "No output file group for images specified, falling back to '%s'", FALLBACK_IMAGE_GRP)
@@ -462,18 +462,18 @@ class OcrdAnybaseocrCropper(Processor):
                     'Operation level %s, but should be "page".', oplevel)
                 break
             file_id = input_file.ID.replace(
-                self.input_file_grp, self.output_file_grp)
+                self.input_file_grp, self.page_grp)
 
             # Use input_file's basename for the new file -
             # this way the files retain the same basenames:
             if file_id == input_file.ID:
-                file_id = concat_padded(self.output_file_grp, n)
+                file_id = concat_padded(self.page_grp, n)
             self.workspace.add_file(
                 ID=file_id,
                 file_grp=page_grp,
                 pageId=input_file.pageId,
                 mimetype=MIMETYPE_PAGE,
-                local_filename=os.path.join(self.output_file_grp,
+                local_filename=os.path.join(self.page_grp,
                                             file_id + '.xml'),
                 content=to_xml(pcgts).encode('utf-8')
             )
