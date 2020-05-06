e5601f2e3ca917e08d2f4610a27b637576ec9fa4
Konstantin Baierer <unixprog@gmail.com>
2020-05-06 18:18:25 +0200

    :package: v0.0.2

---
+++ CHANGELOG.md
@@ -0,0 +1,14 @@
+Change Log
+==========
+Versioned according to [Semantic Versioning](http://semver.org/).
+
+## Unreleased
+
+## [0.0.2] - 2020-05-06
+
+Fixed:
+
+  * Graceful degradation to CPU processing if CUDA not available, #56
+
+<!-- link-labels -->
+[0.0.2]: ../../compare/HEAD...v0.0.2
--- setup.py
+++ setup.py
@@ -1,12 +1,15 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
+import json
+with open('ocrd-tool.json', 'r') as f:
+    version = json.load(f)['version']
 
 setup(
     name='ocrd-anybaseocr',
-    version='0.0.1',
+    version=version,
     author="DFKI",
     author_email="Saqib.Bukhari@dfki.de, Mohammad_mohsin.reza@dfki.de",
-    url="https://github.com/mjenckel/LAYoutERkennung",
+    url="https://github.com/OCR-D/ocrd_anybaseocr",
     license='Apache License 2.0',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
@@ -19,7 +22,7 @@ setup(
         'console_scripts': [
             'ocrd-anybaseocr-binarize           = ocrd_anybaseocr.cli.cli:ocrd_anybaseocr_binarize',
             'ocrd-anybaseocr-deskew             = ocrd_anybaseocr.cli.cli:ocrd_anybaseocr_deskew',
-            'ocrd-anybaseocr-crop               = ocrd_anybaseocr.cli.cli:ocrd_anybaseocr_cropping',        
+            'ocrd-anybaseocr-crop               = ocrd_anybaseocr.cli.cli:ocrd_anybaseocr_cropping',
             'ocrd-anybaseocr-dewarp             = ocrd_anybaseocr.cli.cli:ocrd_anybaseocr_dewarp',
             'ocrd-anybaseocr-tiseg              = ocrd_anybaseocr.cli.cli:ocrd_anybaseocr_tiseg',
             'ocrd-anybaseocr-textline           = ocrd_anybaseocr.cli.cli:ocrd_anybaseocr_textline',
