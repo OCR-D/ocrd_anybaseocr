fe40375e1375dc79ace020377961f6052e97d0d4
Robert Sachunsky <sachunsky@informatik.uni-leipzig.de>
2021-04-22 17:18:02 +0200

    cropping: improve bombast/outdated comment header

--- ocrd_anybaseocr/cli/ocrd_anybaseocr_cropping.py
+++ ocrd_anybaseocr/cli/ocrd_anybaseocr_cropping.py
@@ -1,24 +1,14 @@
 # pylint: disable=invalid-name
-# ======================================================================
-# ====================================
-# README file for Page Cropping component
-# ====================================
-# Filename : ocrd-anyBaseOCR-pagecropping.py
+# A sample image and METS (repo/assets/data/dfki-testdata/data/mets.xml,
+# page becker_quaestio_1586_00013) is provided, see make test.
 
-# Author: Syed Saqib Bukhari, Mohammad Mohsin Reza, Md. Ajraf Rakib
-# Responsible: Syed Saqib Bukhari, Mohammad Mohsin Reza, Md. Ajraf Rakib
-# Contact Email: Saqib.Bukhari@dfki.de, Mohammad_mohsin.reza@dfki.de, Md_ajraf.rakib@dfki.de
-# Note:
-# 1) this work has been done in DFKI, Kaiserslautern, Germany.
-# 2) The parameters values are read from ocrd-anyBaseOCR-parameter.json file. The values can be changed in that file.
-# 3) The command line IO usage is based on "OCR-D" project guidelines (https://ocr-d.github.io/). A sample image file (samples/becker_quaestio_1586_00013.tif) and mets.xml (mets.xml) are provided. The sequence of operations is: binarization, deskewing, cropping and dewarping (or can also be: binarization, dewarping, deskewing, and cropping; depends upon use-case).
+# Workflow should be: binarization, deskewing, cropping and dewarping
+# (but can also be: binarization, dewarping, deskewing, and cropping).
 
-# *********** Method Behaviour ********************
-# This function takes a document image as input and crops/selects the page content
-# area only (that's mean remove textual noise as well as any other noise around page content area)
-# *********** Method Behaviour ********************
+# Takes a document image as input and crops/selects the page frame
+# (text content area), removing textual noise as well as any other
+# noise near the margins (including a ruler placed in the photo).
 
-# *********** LICENSE ********************
 # Copyright 2018 Syed Saqib Bukhari, Mohammad Mohsin Reza, Md. Ajraf Rakib
 # Apache License 2.0
 
@@ -26,10 +16,6 @@
 # and license notices. Contributors provide an express grant of patent rights.
 # Licensed works, modifications, and larger works may be distributed under
 # different terms and without source code.
-
-# *********** LICENSE ********************
-# ======================================================================
-
 
 import os
 from types import SimpleNamespace
@@ -43,7 +29,6 @@ from scipy.stats import linregress
 
 import click
 from ocrd.decorators import ocrd_cli_options, ocrd_cli_wrap_processor
-
 
 from ..constants import OCRD_TOOL
 from ocrd import Processor
