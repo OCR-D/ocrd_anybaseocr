5ca9f9647a8bf438bca6a6bf463ba8534c012e94
Robert Sachunsky <sachunsky@informatik.uni-leipzig.de>
2022-02-20 20:13:29 +0100

    dewarping: improve model loading and rename params…
    - rebase on pix2pixHD#293 (CPU-only option, Torch>=1.0,
      less verbose, arg passing)
    - pass args to pix2pixHD directly (instead of sys.args
      hijacking)
    - no unneccesary verbosity (and only through loggers)
    - move model loading into startup context via `setup` fn
    - rename params:
      * `imgresize` → `resize_mode`,
      * `resizeHeight` → `resize_height`
      * `resizeWidth` → `resize_width`
    - add proper documentation
    - fix region-level results

--- ocrd_anybaseocr/cli/ocrd_anybaseocr_dewarp.py
+++ ocrd_anybaseocr/cli/ocrd_anybaseocr_dewarp.py
@@ -58,36 +58,28 @@ def prepare_data(opt, page_img):
 
 def prepare_options(gpu_id, model_path, resize_or_crop, loadSize, fineSize):
     LOG = getLogger('OcrdAnybaseocrDewarper')
-    # XXX https://github.com/OCR-D/ocrd_anybaseocr/pull/62#discussion_r450232164
-    # The problem was with how BaseOptions.parse is implemented in pix2pixHD based on
-    # argparse. I cannot explain why but the approach to let pix2pixHD fill the
-    # TestOptions instance with argparse default values and then modifying the
-    # instance did not work, the overrides were simply ignored. The only way I got
-    # pix2pixHD to reliably pick up the overrides was this sys.argv approach. It's
-    # ugly, true, but so is using argparse as an API. At least this way, it is
-    # uniform as you say.
-    sys.argv = ['python']
-    sys.argv.extend(['--gpu_ids', str(gpu_id)])
-    sys.argv.extend(['--nThreads', str(1)])   # test code only supports nThreads = 1
-    sys.argv.extend(['--batchSize', str(1)])  # test code only supports batchSize = 1
-    sys.argv.extend(['--serial_batches'])  # no shuffle
-    sys.argv.extend(['--no_flip'])  # no flip
-    sys.argv.extend(['--dataroot', dataroot])
-    sys.argv.extend(['--checkpoints_dir', str(model_path.parents[1])])
-    sys.argv.extend(['--name', model_path.parents[0].name])
-    sys.argv.extend(['--label_nc', str(0)])
-    sys.argv.extend(['--no_instance'])
-    sys.argv.extend(['--resize_or_crop', resize_or_crop])
-    sys.argv.extend(['--n_blocks_global', str(10)])
-    sys.argv.extend(['--n_local_enhancers', str(2)])
-    sys.argv.extend(['--loadSize', str(loadSize)])
-    sys.argv.extend(['--fineSize', str(fineSize)])
-    sys.argv.extend(['--model', 'pix2pixHD'])
-    sys.argv.extend(['--verbose'])
-    LOG.debug("Options passed to pix2pixHD: %s", sys.argv)
+    # we cannot use TestOptions instances directly, because its parse()
+    # does some nontrivial postprocessing (which we do not want to redo here)
+    args = []
+    args.extend(['--gpu_ids', str(gpu_id)])
+    args.extend(['--nThreads', str(1)])   # test code only supports nThreads = 1
+    args.extend(['--batchSize', str(1)])  # test code only supports batchSize = 1
+    args.extend(['--serial_batches'])  # no shuffle
+    args.extend(['--no_flip'])  # no flip
+    args.extend(['--checkpoints_dir', str(model_path.parents[1])])
+    args.extend(['--name', model_path.parents[0].name])
+    args.extend(['--label_nc', str(0)]) # number of input label channels (just RGB if zero)
+    args.extend(['--no_instance']) # no instance maps as input
+    args.extend(['--resize_or_crop', resize_or_crop])
+    args.extend(['--n_blocks_global', str(10)])
+    args.extend(['--n_local_enhancers', str(2)])
+    args.extend(['--loadSize', str(loadSize)])
+    args.extend(['--fineSize', str(fineSize)])
+    args.extend(['--model', 'pix2pixHD'])
+    #args.extend(['--verbose'])
+    LOG.debug("Options passed to pix2pixHD: %s", args)
     opt = TestOptions()
-    opt.initialize()
-    opt = opt.parse(save=False)
+    opt = opt.parse(args=args, save=False, silent=True)
 
     model = create_model(opt)
 
@@ -99,36 +91,56 @@ class OcrdAnybaseocrDewarper(Processor):
         kwargs['ocrd_tool'] = OCRD_TOOL['tools'][TOOL]
         kwargs['version'] = OCRD_TOOL['version']
         super(OcrdAnybaseocrDewarper, self).__init__(*args, **kwargs)
+        if hasattr(self, 'output_file_grp') and hasattr(self, 'parameter'):
+            # processing context
+            self.setup()
 
-
-    def process(self):
+    def setup(self):
         LOG = getLogger('OcrdAnybaseocrDewarper')
-
-        assert_file_grp_cardinality(self.input_file_grp, 1)
-        assert_file_grp_cardinality(self.output_file_grp, 1)
-
         if self.parameter['gpu_id'] > -1 and not torch.cuda.is_available():
             LOG.warning("torch cannot detect CUDA installation.")
             self.parameter['gpu_id'] = -1
 
         model_path = Path(self.resolve_resource(self.parameter['model_path']))
         if not model_path.is_file():
-            LOG.error("""\
-                    pix2pixHD model file was not found at '%s'. Make sure this file exists.
-                """ % model_path)
+            LOG.error("pix2pixHD model file was not found at '%s'", model_path)
             sys.exit(1)
-
-        opt, model = prepare_options(
+        self.opt, self.model = prepare_options(
             gpu_id=self.parameter['gpu_id'],
-            dataroot=str(Path(self.workspace.directory, self.input_file_grp)),
             model_path=model_path,
-            resize_or_crop=self.parameter['imgresize'],
-            loadSize=self.parameter['resizeHeight'],
-            fineSize=self.parameter['resizeWidth'],
+            resize_or_crop=self.parameter['resize_mode'],
+            loadSize=self.parameter['resize_height'],
+            fineSize=self.parameter['resize_width'],
         )
 
+    def process(self):
+        """Dewarp pages of the workspace via pix2pixHD (conditional GANs)
+
+        Open and deserialise each PAGE input file and its respective image,
+        then iterate over its segment hierarchy down to the requested
+        ``operation_level``.
+
+        Next, get the binarized image according to the layout annotation
+        (from the alternative image of the segment, or by cropping and
+        deskewing from the parent image as annotated).
+
+        Then pass the image to the preloaded pix2pixHD model for inference.
+        (It will be resized and/or cropped according to ``resize_width``,
+        ``resize_height`` and ``resize_mode`` prior to decoding, and the
+        result will be resized to match the original.)
+
+        After decoding, add the new image file to the output fileGrp for
+        the same pageId (using a file ID with suffix ``.IMG-DEW``).
+        Reference the new image file in the AlternativeImage of the segment.
+
+        Produce a new output file by serialising the resulting hierarchy.
+        """
+        LOG = getLogger('OcrdAnybaseocrDewarper')
+        assert_file_grp_cardinality(self.input_file_grp, 1)
+        assert_file_grp_cardinality(self.output_file_grp, 1)
+
         oplevel = self.parameter['operation_level']
-        for (n, input_file) in enumerate(self.input_files):
+        for input_file in self.input_files:
             page_id = input_file.pageId or input_file.ID
             LOG.info("INPUT FILE %s", page_id)
 
@@ -136,30 +148,25 @@ class OcrdAnybaseocrDewarper(Processor):
             self.add_metadata(pcgts)
             page = pcgts.get_Page()
 
-            try:
-                page_image, page_xywh, _ = self.workspace.image_from_page(
-                    page, page_id, feature_filter='dewarped', feature_selector='binarized')  # images should be deskewed and cropped
-            except Exception:
-                page_image, page_xywh, _ = self.workspace.image_from_page(
-                    page, page_id, feature_filter='dewarped')  # images should be deskewed and cropped
+            page_image, page_xywh, _ = self.workspace.image_from_page(
+                page, page_id,
+                # images SHOULD be deskewed and cropped, and MUST be binarized
+                feature_filter='dewarped', feature_selector='binarized')
             if oplevel == 'page':
-                dataset = prepare_data(opt, page_image)
-                orig_img_size = page_image.size
                 self._process_segment(
-                    model, dataset, page, page_xywh, page_id, input_file, orig_img_size, n)
+                    prepare_data(self.opt, page_image), page, page_xywh, page_image.size, input_file)
             else:
                 regions = page.get_TextRegion() + page.get_TableRegion()  # get all regions?
                 if not regions:
                     LOG.warning("Page '%s' contains no text regions", page_id)
                 for _, region in enumerate(regions):
                     region_image, region_xywh = self.workspace.image_from_segment(
-                        region, page_image, page_xywh)
+                        region, page_image, page_xywh,
+                        # images SHOULD be deskewed and cropped, and MUST be binarized
+                        feature_filter='dewarped', feature_selector='binarized')
                     # TODO: not tested on regions
-                    # TODO: region has to exist as a physical file to be processed by pix2pixHD
-                    dataset = prepare_data(opt, region_image)
-                    orig_img_size = region_image.size
                     self._process_segment(
-                        model, dataset, page, region_xywh, region.id, input_file, orig_img_size, n)
+                        prepare_data(self.opt, region_image), region, region_xywh, region_image.size, input_file)
 
             file_id = make_file_id(input_file, self.output_file_grp)
             pcgts.set_pcGtsId(file_id)
@@ -172,7 +179,7 @@ class OcrdAnybaseocrDewarper(Processor):
                 content=to_xml(pcgts).encode('utf-8')
             )
 
-    def _process_segment(self, model, dataset, page, page_xywh, page_id, input_file, orig_img_size, n):
+    def _process_segment(self, dataset, segment, coords, orig_img_size, input_file):
         for _, data in enumerate(dataset):
             w, h = orig_img_size
             generated = self.model.inference(data['label'], data['inst'], data['image'])
@@ -193,9 +200,9 @@ class OcrdAnybaseocrDewarper(Processor):
                                                        file_id,
                                                        page_id=input_file.pageId,
                                                        file_grp=self.output_file_grp,
-                                                      )
-            page.add_AlternativeImage(AlternativeImageType(
-                filename=file_path, comments=page_xywh['features']))
+            )
+            segment.add_AlternativeImage(AlternativeImageType(
+                filename=file_path, comments=coords['features']))
 
 @click.command()
 @ocrd_cli_options
